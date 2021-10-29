16ab2d879f2afbf12112eef124ee90240e3cf369
AlexFradle <alexandergoodall123@gmail.com>
2021-10-29 22:11:36 +0100

    add level lines and table

--- src/components/LineGraph.jsx
+++ src/components/LineGraph.jsx
@@ -3,10 +3,29 @@ import PropTypes from "prop-types";
 import Plot from "react-plotly.js";
 import Papa from "papaparse/papaparse";
 import fitnessData from "../fitness.json";
-import {linearRegression} from "simple-statistics"
+import coeffs from "../coefficients.json";
+import colours from "../colours.json";
+import {linearRegression} from "simple-statistics";
+import "./LineGraph.css"
 
 let graphData = {};
 const allExercises = [];
+const exerciseKey = {
+    "Back Squat": 0,
+    "Front Squat": 1,
+    "Deadlift": 2,
+    "Sumo Deadlift": 3,
+    "Power Clean": 4,
+    "Bench Press": 5,
+    "Incline Bench Press": 6,
+    "Dip": 7,
+    "Overhead Press": 8,
+    "Push Press": 9,
+    "Snatch Press": 10,
+    "Chin-up": 11,
+    "Pull-up": 12,
+    "Pendlay Row": 13
+};
 
 const LineGraph = props => {
     const highest = obj => Math.max(...obj.weight);
@@ -80,6 +99,7 @@ const LineGraph = props => {
             "Deadlift (Barbell)": "Deadlift",
             "Sumo Deadlift (Barbell)": "Sumo Deadlift",
             "Squat (Barbell)": "Back Squat",
+            "Front Squat (Barbell)": "Front Squat",
             "Pull Up": "Pull-up",
             "Chin Up": "Chin-up",
             "Overhead Press (Barbell)": "Overhead Press"
@@ -87,6 +107,17 @@ const LineGraph = props => {
 
         if (exerciseMap[target.value] !== undefined) {
             setAverageGraphData(exerciseMap[target.value]);
+        } else {
+            setAverageData({
+                "untrained": 0,
+                "novice": 0,
+                "intermediate": 0,
+                "proficient": 0,
+                "advanced": 0,
+                "exceptional": 0,
+                "elite": 0,
+                "world class": 0,
+            });
         }
     };
 
@@ -114,25 +145,10 @@ const LineGraph = props => {
 
     const setAverageGraphData = exerciseName => {
         let i = 1;
-        const exerciseKey = {
-            "Back Squat": 0,
-            "Front Squat": 1,
-            "Deadlift": 2,
-            "Sumo Deadlift": 3,
-            "Power Clean": 4,
-            "Bench Press": 5,
-            "Incline Bench Press": 6,
-            "Dip": 7,
-            "Overhead Press": 8,
-            "Push Press": 9,
-            "Snatch Press": 10,
-            "Chin-up": 11,
-            "Pull-up": 12,
-            "Pendlay Row": 13
-        };
         const temp = {};
         for (const key in averageData) {
             temp[key] = parseInt(fitnessData[gender][weight][exerciseKey[exerciseName]][i].match(/-?\d+/), 10);
+            if (age < 23 || age > 40) temp[key] = Math.ceil(temp[key] / coeffs[age]);
             i += 1;
         }
         setAverageData(temp);
@@ -152,17 +168,148 @@ const LineGraph = props => {
         console.log(linearRegression([x, y]));
     };
 
+    const getShapes = () => {
+        const newLines = [
+            {
+                type: 'line',
+                xref: "paper",
+                x0: 0,
+                y0: yMax,
+                x1: 1,
+                y1: yMax,
+                line: {
+                    color: "#FF0000",
+                }
+            }
+        ];
+        if (Object.values(averageData).reduce((acc, cv) => acc + cv) !== 0) {
+            for (const [name, num] of Object.entries(averageData)) {
+                newLines.push({
+                    type: 'line',
+                    xref: "paper",
+                    x0: 0,
+                    y0: num,
+                    x1: 1,
+                    y1: num,
+                    line: {
+                        color: colours[name],
+                    },
+                    opacity: 0.5
+                });
+            }
+        }
+        return newLines;
+    };
+
+    const getAnnotations = () => {
+        const newAnnotations = [
+            {
+                xref: 'paper',
+                x: 0,
+                xanchor: 'right',
+                y: yMax,
+                yanchor: 'bottom',
+                text: `${yMax}`,
+                showarrow: false,
+                font: {color: "#FF0000"}
+            }
+        ];
+        if (Object.values(averageData).reduce((acc, cv) => acc + cv) !== 0) {
+            for (const [name, num] of Object.entries(averageData)) {
+                newAnnotations.push({
+                    xref: 'paper',
+                    x: 0,
+                    xanchor: 'right',
+                    y: num,
+                    yanchor: 'bottom',
+                    text: `${num}`,
+                    showarrow: false,
+                    font: {color: colours[name], size: 10}
+                });
+            }
+        }
+        return newAnnotations;
+    };
+
+    const getLineData = () => {
+        const arr = [
+            {
+                x: xData,
+                y: yData,
+                connectgaps: true,
+                type: "scatter",
+                mode: "lines+markers",
+                marker: { color: props.lineColor }
+            }
+        ];
+        if (1 === 2) {
+            arr.push({
+                x: repXData,
+                y: repYData,
+                connectgaps: true,
+                yaxis: "y2",
+                type: "scatter",
+                mode: "lines+markers",
+                opacity: 0.2,
+                marker: { color: "#009bff" }
+            });
+        }
+        return arr;
+    };
+
+    const getLayoutData = () => {
+        const obj = {
+            width: 1500,
+            height: 700,
+            plot_bgcolor: props.backgroundColor,
+            paper_bgcolor: props.backgroundColor,
+            titlefont: {color: props.fontColor},
+            xaxis: {
+                gridcolor: props.gridColor,
+                linecolor: props.fontColor,
+                tickfont: {color: props.fontColor}
+            },
+            yaxis: {
+                title: "Highest Weight (kg)",
+                gridcolor: props.gridColor,
+                linecolor: props.fontColor,
+                titlefont: {color: props.fontColor},
+                tickfont: {color: props.fontColor},
+            },
+            shapes: getShapes(),
+            annotations: getAnnotations()
+        };
+        if (1 === 2) {
+            obj.yaxis2 = {
+                title: 'Total Reps',
+                titlefont: {color: "#009bff"},
+                tickfont: {color: "#009bff"},
+                overlaying: 'y',
+                side: 'right',
+                showgrid: false
+            };
+        }
+
+        return obj;
+    };
+
     return (
         <div>
-            <select onChange={handleGenderSelectChange}>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-            </select>
-            <input type="number" onChange={handleWeightChange} min="20" max="200"/>
-            <input type="number" onChange={handleAgeChange} min="14" max="90"/>
+            <div className="menu">
+                <label htmlFor="gender-select">Gender Selection: </label>
+                <select onChange={handleGenderSelectChange} id="gender-select">
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                </select>
+                <label htmlFor="weight-input">Weight (kg): </label>
+                <input type="number" onChange={handleWeightChange} min="20" max="200" id="weight-input"/>
+                <label htmlFor="age-input">Age: </label>
+                <input type="number" onChange={handleAgeChange} min="14" max="90" id="age-input"/>
 
-            <input type="file" name="file" onChange={handleFileChange}/>
-            <button onClick={setGraphData}>press</button>
+                <input type="file" name="file" onChange={handleFileChange}/>
+                <button onClick={setGraphData}>Load</button>
+            </div>
+
             <button onClick={makeLine}>press line</button>
             <br/>
             <select onChange={handleExerciseSelectChange}>
@@ -171,77 +318,27 @@ const LineGraph = props => {
                 })}
             </select>
             <Plot
-                data={[
-                    {
-                        x: xData,
-                        y: yData,
-                        connectgaps: true,
-                        type: "scatter",
-                        mode: "lines+markers",
-                        marker: { color: props.lineColor }
-                    },
-                    {
-                        x: repXData,
-                        y: repYData,
-                        connectgaps: true,
-                        yaxis: "y2",
-                        type: "scatter",
-                        mode: "lines+markers",
-                        marker: { color: "#009bff" }
-                    },
-                ]}
-                layout={{
-                    width: 1000,
-                    height: 500,
-                    plot_bgcolor: props.backgroundColor,
-                    paper_bgcolor: props.backgroundColor,
-                    titlefont: {color: props.fontColor},
-                    xaxis: {
-                        gridcolor: props.gridColor,
-                        linecolor: props.fontColor,
-                        tickfont: {color: props.fontColor}
-                    },
-                    yaxis: {
-                        title: "Highest Weight (kg)",
-                        gridcolor: props.gridColor,
-                        linecolor: props.fontColor,
-                        titlefont: {color: props.fontColor},
-                        tickfont: {color: props.fontColor}
-                    },
-                    yaxis2: {
-                        title: 'Total Reps',
-                        titlefont: {color: "#009bff"},
-                        tickfont: {color: "#009bff"},
-                        overlaying: 'y',
-                        side: 'right'
-                    },
-                    shapes: [
-                        {
-                            type: 'line',
-                            xref: "paper",
-                            x0: 0,
-                            y0: yMax,
-                            x1: 1,
-                            y1: yMax,
-                            line: {
-                                color: "#FF0000",
-                            }
-                        },
-                    ],
-                    annotations: [
-                        {
-                            xref: 'paper',
-                            x: 0,
-                            xanchor: 'right',
-                            y: yMax,
-                            yanchor: 'bottom',
-                            text: `${yMax}`,
-                            showarrow: false,
-                            font: {color: "#FF0000"}
-                        }
-                    ]
-                }}
+                data={getLineData()}
+                layout={getLayoutData()}
             />
+            <table>
+                <thead>
+                    <tr>
+                        <td>Strength Level</td>
+                        <td>Weight (kg)</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {Object.entries(averageData).map(([name, num]) => {
+                        return (
+                            <tr>
+                                <td style={{color: colours[name]}}>{name}</td>
+                                <td>{num}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
         </div>
     );
 }
